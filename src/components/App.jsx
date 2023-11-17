b0a8be73b5dc7aaebfa6d1a9ed175c00f05ff3b9
Annargb <annasamoylenko2015@gmail.com>
2023-11-17 21:00:10 +0100

    use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactsForm } from './ContactsForm-component/ContactsForm';
 import { ContactsList } from './ContactsList-component/ContactsList';
@@ -12,110 +11,86 @@ import {
   AccentMainTitle,
 } from './App.styled';
 import { InfoMessage } from './InfoMessage-component/InfoMessage';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
 
-  addContact = newContact => {
+  const addContact = newContact => {
     const contact = {
       ...newContact,
       id: nanoid(),
     };
 
-    const contactAlreadyExist = this.state.contacts.some(
+    const contactAlreadyExist = contacts.some(
       item => item.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
     contactAlreadyExist
       ? alert(`${newContact.name} is already in contacts!`)
-      : this.setState(prevState => {
-          return {
-            contacts: [...prevState.contacts, contact],
-          };
-        });
+      : setContacts(prevState => [...prevState, contact]);
   };
 
-  contactsFilter = searchContact => {
-    this.setState({ filter: searchContact });
+  const contactsFilter = searchContact => setFilter(searchContact);
+
+  const deleteContact = contactId => {
+    setContacts(prevState => prevState.filter(item => item.id !== contactId));
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(item => item.id !== contactId),
-      };
-    });
-  };
-
-  componentDidMount() {
-    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-
-    if (savedContacts) {
-      this.setState({ contacts: savedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
-
-    const variantOfText = () => {
-      if (!contacts.length) {
-        return (
-          <InfoMessage
-            text="There are no contacts here 😲"
-            $variant="primary"
-          />
-        );
-      } else if (contacts.length && !visibleContacts.length) {
-        return (
-          <>
-            <Filter filter={filter} onUpdateContacts={this.contactsFilter} />
-            <InfoMessage
-              text="Sorry, we didn't find any contacts for this request 😢"
-              $variant="secondary"
-            />
-          </>
-        );
-      } else {
-        return (
-          <>
-            <Filter filter={filter} onUpdateContacts={this.contactsFilter} />
-            <ContactsList
-              contacts={visibleContacts}
-              onDelete={this.deleteContact}
-            />
-          </>
-        );
+  useEffect(
+    prevState => {
+      if (contacts !== prevState) {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
       }
-    };
+    },
+    [contacts]
+  );
 
-    return (
-      <Wrapper>
-        <Section>
-          <MainTitle>
-            Phone<AccentMainTitle>book</AccentMainTitle>
-          </MainTitle>
-          <ContactsForm onAddContacts={this.addContact} />
-        </Section>
-        <Section>
-          <ContactsTitle>Contacts</ContactsTitle>
-          {variantOfText()}
-        </Section>
-        <GlobalStyle />
-      </Wrapper>
-    );
-  }
-}
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+  );
+
+  const variantOfText = () => {
+    if (!contacts.length) {
+      return (
+        <InfoMessage text="There are no contacts here 😲" $variant="primary" />
+      );
+    } else if (contacts.length && !visibleContacts.length) {
+      return (
+        <>
+          <Filter filter={filter} onUpdateContacts={contactsFilter} />
+          <InfoMessage
+            text="Sorry, we didn't find any contacts for this request 😢"
+            $variant="secondary"
+          />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Filter filter={filter} onUpdateContacts={contactsFilter} />
+          <ContactsList contacts={visibleContacts} onDelete={deleteContact} />
+        </>
+      );
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Section>
+        <MainTitle>
+          Phone<AccentMainTitle>book</AccentMainTitle>
+        </MainTitle>
+        <ContactsForm onAddContacts={addContact} />
+      </Section>
+      <Section>
+        <ContactsTitle>Contacts</ContactsTitle>
+        {variantOfText()}
+      </Section>
+      <GlobalStyle />
+    </Wrapper>
+  );
+};
